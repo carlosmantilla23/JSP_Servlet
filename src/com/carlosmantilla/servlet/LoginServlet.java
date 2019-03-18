b88d233c107aa373c4306012a5e13c544770c1fa
carlosmantilla23 <carlos.mantilla.jaimes@gmail.com>
2019-03-17 22:42:09 -0500

    Servlets JSP

---
+++ src/com/carlosmantilla/servlet/LoginServlet.java
@@ -0,0 +1,55 @@
+package com.carlosmantilla.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet(urlPatterns="/loginTest")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//Procesar la información
+		String usuario = request.getParameter("usuario");
+		String empresa = request.getParameter("empresa");
+		PrintWriter pw = response.getWriter();
+		if (usuario != null && empresa != null) {
+			if (empresa.equalsIgnoreCase("platzi")) {
+			pw.println("Bienvenido a Platzi");
+		} else {
+			pw.println("Bienvenido....");
+		}
+	}else {
+			pw.println("Usuario incorrecto");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
